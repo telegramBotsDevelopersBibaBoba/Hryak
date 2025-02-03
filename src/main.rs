716150182f9e2771087ebd2e9b255739f4f0184c
klewy <cigankov807@gmail.com>
2025-02-03 16:55:57 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod controllers;
+mod db;
+
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
 }
