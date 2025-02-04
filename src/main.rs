da9cc3ce26c2c6ea40c54a1fef04342f20287654
klewy <cigankov807@gmail.com>
2025-02-04 12:08:00 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-mod controllers;
-mod db;
+use std::str::FromStr;
 
-use teloxide::prelude::*;
+use teloxide::{
+    payloads::EditMessageText,
+    prelude::*,
+    sugar::bot::BotMessagesExt,
+    types::{
+        InlineQueryResult, InlineQueryResultArticle, InputMessageContent, InputMessageContentText,
+        ParseMode,
+    },
+    RequestError,
+};
+
+mod config;
+mod db;
+mod handlers;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
+    log::info!("Starting inline bot...");
 
-    let bot = Bot::from_env();
+    let bot = Bot::from_env(); // Setting up bot from TELOXIDE_TOKEN env variable (P.S 'export TELOXIDE_TOKEN=<token>' in terminal)
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
-    .await;
+    // Just boilerplate stuff
+    let handler = dptree::entry()
+        .branch(Update::filter_inline_query().endpoint(handlers::filter::filter_inline_commands));
+
+    Dispatcher::builder(bot, handler)
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
 }
