013e2b013426b79a5caa1cfaff4e722b9311d1d8
klewy <cigankov807@gmail.com>
2025-02-05 17:22:41 +0300

    upd

--- src/db/pigdb.rs
+++ src/db/pigdb.rs
@@ -4,17 +4,34 @@ use sqlx::Row;
 
 pub async fn create_pig(pool: &MySqlPool, user_id: u64) -> anyhow::Result<()> {
     sqlx::query("INSERT INTO pigs (user_id) VALUES (?)")
-    .bind(user_id)
-    .execute(pool).await?;
+        .bind(user_id)
+        .execute(pool)
+        .await?;
 
     Ok(())
 }
 
+pub async fn pig_exists(pool: &MySqlPool, user_id: u64) -> bool {
+    match sqlx::query("SELECT * FROM pigs WHERE user_id = ?")
+        .bind(user_id)
+        .fetch_one(pool)
+        .await
+    {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
 pub async fn get_pig_weight(pool: &MySqlPool, user_id: u64) -> anyhow::Result<f32> {
+    if !pig_exists(pool, user_id).await {
+        create_pig(pool, user_id).await?;
+    }
+
     let row = sqlx::query("SELECT weight FROM pigs WHERE user_id = ?")
-    .bind(user_id)
-    .fetch_one(pool).await?;
-    
+        .bind(user_id)
+        .fetch_one(pool)
+        .await?; // If throws -> user does not exist
+
     let weight = row.try_get::<f32, _>(0)?;
 
     Ok(weight)
--- src/handlers/filter.rs
+++ src/handlers/filter.rs
@@ -1,31 +1,48 @@
 use std::{str::FromStr, time::Duration};
 
+use anyhow::anyhow;
 use futures::FutureExt;
 use sqlx::MySqlPool;
 use teloxide::{
     prelude::{Request, Requester},
     respond,
     types::{
-        InlineQuery, InlineQueryResult, InlineQueryResultArticle, InputMessageContent,
-        InputMessageContentText,
+        InlineKeyboardButton, InlineKeyboardMarkup, InlineQuery, InlineQueryResult,
+        InlineQueryResultArticle, InputMessageContent, InputMessageContentText,
     },
     Bot, RequestError,
 };
 use tokio::time::sleep;
 
-use crate::{config::commands::InlineCommands, db::{pigdb::get_pig_weight, userdb}};
+use crate::{
+    config::commands::InlineCommands,
+    db::{pigdb::get_pig_weight, userdb},
+};
 
-pub async fn filter_inline_commands(bot: Bot, q: InlineQuery, pool: MySqlPool) -> Result<(), RequestError> {
+#[derive(thiserror::Error, Debug)]
+pub enum MyError {
+    #[error(transparent)]
+    RequestError(#[from] teloxide::RequestError),
+
+    #[allow(unused)]
+    #[error("unknown error: {0}")]
+    Unknown(String),
+}
+
+pub async fn filter_inline_commands(
+    bot: Bot,
+    q: InlineQuery,
+    pool: MySqlPool,
+) -> Result<(), RequestError> {
     let command_str = &q.query; // Extracting a command from the query (we'll have to parse it later for arguments I think tho)
 
     // Storing a function based on what query is that, if empty -> show 'help'
     let function = match InlineCommands::from_str(&command_str) {
         Ok(command) => match command {
-            InlineCommands::Hryak => inline_get_hryak_size(bot, &q, &pool).boxed(),
+            InlineCommands::Hryak => inline_all_commands(bot, &q, &pool).boxed(),
         },
-        Err(_) => inline_help(bot, &q).boxed(),
+        Err(_) => inline_all_commands(bot, &q, &pool).boxed(),
     };
-    
     // Executing the function
     let resp = function.await;
 
@@ -37,43 +54,66 @@ pub async fn filter_inline_commands(bot: Bot, q: InlineQuery, pool: MySqlPool) -
     Ok(())
 }
 
-pub async fn inline_help(bot: Bot, q: &InlineQuery) -> Result<(), RequestError> {
-    // Creating an inline button? 
+pub async fn inline_all_commands(
+    bot: Bot,
+    q: &InlineQuery,
+    pool: &MySqlPool,
+) -> anyhow::Result<()> {
+    let hryak = inline_hryak_weight_article(q, pool).await.unwrap();
+    let help = inline_help_article(q, pool).await.unwrap();
+
+    let articles = vec![
+        InlineQueryResult::Article(hryak),
+        InlineQueryResult::Article(help),
+    ];
+
+    let response = bot.answer_inline_query(&q.id, articles).send().await?; // Showing all suitable inline buttons
+    Ok(())
+}
+
+pub async fn inline_hryak_weight_article(
+    q: &InlineQuery,
+    pool: &MySqlPool,
+) -> anyhow::Result<InlineQueryResultArticle> {
+    let mass = match get_pig_weight(pool, q.from.id.0).await {
+        Ok(mass) => mass,
+        Err(why) => {
+            userdb::create_user(pool, q.from.id.0, &q.from.first_name).await?;
+            return Err(anyhow!("{}", why));
+        }
+    };
+
+    let hrundel_weight = InlineQueryResultArticle::new(
+        "02".to_string(),
+        "Узнать массу хряка".to_string(),
+        InputMessageContent::Text(InputMessageContentText::new(
+            format!("Размер хряка: {} кг.", mass.to_string())
+        )),
+    )
+    .description("Hryak")
+    .thumbnail_url("https://sputnik.kz/img/858/06/8580645_0:0:3117:2048_600x0_80_0_1_81d5b1f42e05e39353aa388a4e55cb34.jpg".parse().unwrap());
+
+    Ok(hrundel_weight)
+}
+pub async fn inline_help_article(
+    q: &InlineQuery,
+    pool: &MySqlPool,
+) -> anyhow::Result<InlineQueryResultArticle> {
+    let button =
+        InlineKeyboardButton::switch_inline_query_current_chat("Узнать массу хряка", "hryak");
     let help = InlineQueryResultArticle::new(
         "01".to_string(),
         "Узнать все доступные команды".to_string(),
-        InputMessageContent::Text(InputMessageContentText::new("Вот список:")),
+        InputMessageContent::Text(InputMessageContentText::new(
+            "Вот список доступных комманд:",
+        )),
     )
     .description("Узнай все доступные команды")
     .thumbnail_url(
         "https://thumbs.dreamstime.com/z/lot-pigs-d-rendered-illustration-127843482.jpg"
             .parse()
             .unwrap(),
-    );
-
-    let results: Vec<InlineQueryResult> = vec![InlineQueryResult::Article(help)]; 
-    let response = bot.answer_inline_query(&q.id, results).send().await?; // Showing all suitable inline buttons
-
-    Ok(())
-}
-
-pub async fn inline_get_hryak_size(bot: Bot, q: &InlineQuery, pool: &MySqlPool) -> Result<(), RequestError> {
-    let mass = get_pig_weight(pool, q.from.id.0).await.unwrap();
-    // TODO: Better error handling
-
-    println!("done that");
-    let hrundel_weight = InlineQueryResultArticle::new(
-        "02".to_string(),
-        "Узнать массу хряка".to_string(),
-        InputMessageContent::Text(InputMessageContentText::new(
-            mass.to_string()
-        )),
     )
-    .description("Hryak")
-    .thumbnail_url("https://sputnik.kz/img/858/06/8580645_0:0:3117:2048_600x0_80_0_1_81d5b1f42e05e39353aa388a4e55cb34.jpg".parse().unwrap());
-
-    let results: Vec<InlineQueryResult> = vec![InlineQueryResult::Article(hrundel_weight)];
-
-    let response = bot.answer_inline_query(&q.id, results).send().await?;
-    Ok(())
+    .reply_markup(InlineKeyboardMarkup::new([[button]]));
+    Ok(help)
 }
