0641fb4cede5930ad1c53434041345e94dc09baf
boyarishnik <boyarishnik3232@gmail.com>
2025-02-12 23:51:00 +0300

    did some shop magic

--- src/controllers/shop.rs
+++ src/controllers/shop.rs
@@ -1,4 +1,7 @@
 use sqlx::{mysql::MySqlRow, Row};
+use teloxide::types::InlineKeyboardButton;
+use crate::ser_command;
+use std::fmt::Display;
 
 pub struct FoodOffer {
     id: u64,
@@ -23,5 +26,74 @@ impl FoodOffer {
             title,
             description,
         })
+    }
+}
+
+impl Display for FoodOffer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}$ - {} ({}Ккал): {}", self.price, self.title, self.nutrition, self.description)
+    }
+}
+
+pub struct ImprovementOffer {
+    id: u64,
+    title: String,
+    price: f64,
+    description: String,
+    improvement_type: String, // Используем String для enum
+}
+
+impl ImprovementOffer {
+    pub fn from_mysql_row(row: MySqlRow) -> anyhow::Result<Self> {
+        let id = row.try_get::<u64, _>(0)?;
+        let title = row.try_get::<String, _>(1)?;
+        let price = row.try_get::<f64, _>(2)?;
+        let description: String = row.try_get::<Option<String>, _>(3)?
+            .unwrap_or(String::from("Нет описания")); // Обрабатываем NULL
+        let improvement_type = row.try_get::<String, _>(4)?;
+
+        Ok(Self {
+            id,
+            title,
+            price,
+            description,
+            improvement_type,
+        })
+    }
+}
+
+impl Display for ImprovementOffer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} - {} ({}): {}", self.price, self.title, self.improvement_type, self.description)
+    }
+}
+
+pub enum OfferType {
+    Improvement,
+    Food,
+}
+
+pub enum Offer {
+    Improvement(ImprovementOffer),
+    Food(FoodOffer),
+}
+
+impl Offer {
+    pub fn get_button(&self, index: usize) -> InlineKeyboardButton {
+        let (id, title, offer_type) = match self {
+            Self::Food(item) => (item.id, item.title.clone(), "food"),
+            Self::Improvement(item) => (item.id, item.title.clone(), "improvement"),
+        };
+        InlineKeyboardButton::callback(
+            format!("{}) {}", index, title), 
+            ser_command!("shop", "food", &id.to_string())
+        )
+    }
+
+    pub fn get_info(&self, index: usize) -> String {
+        match self {
+            Self::Food(item) => format!("{}) {}", index, item),
+            Self::Improvement(item) => format!("{}) {}", index, item),
+        }
     }
 }
--- src/db/shopdb.rs
+++ src/db/shopdb.rs
@@ -1,4 +1,4 @@
-use crate::controllers::shop::FoodOffer;
+use crate::controllers::shop::{FoodOffer, ImprovementOffer, OfferType, Offer};
 use sqlx::{MySqlPool, Row};
 
 pub async fn get_food_offer_by_id(pool: &MySqlPool, offer_id: u64) -> anyhow::Result<FoodOffer> {
@@ -8,4 +8,20 @@ pub async fn get_food_offer_by_id(pool: &MySqlPool, offer_id: u64) -> anyhow::Re
         .await?;
 
     Ok(FoodOffer::from_mysql_row(row)?)
+}
+
+pub async fn get_improvement_offer_by_id(pool: &MySqlPool, improvement_id: u64) -> anyhow::Result<ImprovementOffer> {
+    let row = sqlx::query("SELECT * FROM shop_improvements WHERE id = ?")
+        .bind(improvement_id)
+        .fetch_one(pool)
+        .await?;
+
+    Ok(ImprovementOffer::from_mysql_row(row)?)
+}
+
+pub async fn get_offer(pool: &MySqlPool, offer_type: OfferType, item_id: u64) -> anyhow::Result<Offer> {
+    Ok(match offer_type {
+        OfferType::Food => Offer::Food(get_food_offer_by_id(pool, item_id).await?),
+        OfferType::Improvement => Offer::Improvement(get_improvement_offer_by_id(pool, item_id).await?),
+    })
 }
